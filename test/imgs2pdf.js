f0843928290460d98f83f239af759e54e858664b
skt-t1-byungi <tiniwz@gmail.com>
2019-05-30 18:09:13 +0900

    wip

--- test/imgs2pdf.js
+++ test/imgs2pdf.js
@@ -1,18 +1,17 @@
 const test = require('ava')
 const path = require('path')
-const bundle = require('../lib/bundle')
+const imgs2pdf = require('../lib/imgs2pdf')
 const tempfile = require('tempfile')
 const fs = require('fs')
 
-const COVER = path.join(__dirname, 'fixtures/cover.jpg')
-const IMGS = [...Array(2).keys()].map(n => path.join(__dirname, `fixtures/${n}.jpg`))
+const IMGS = [...Array(3).keys()].map(n => path.join(__dirname, `fixtures/${n}.jpg`))
 const DEST = tempfile()
 
 test.after.always(() => fs.unlink(DEST, () => {}))
 
-test('bundle', async t => {
+test('imgs2pdf', async t => {
     t.false(fs.existsSync(DEST))
-    await bundle({ pages: IMGS, cover: COVER }, DEST)
+    await imgs2pdf(IMGS, DEST)
     t.true(fs.existsSync(DEST))
     fs.copyFileSync(DEST, './examples/bundle.pdf')
 })
