2b10548dc7492e89a377212abdabdb6791926d59
skt-t1-byungi <tiniwz@gmail.com>
2019-05-30 13:32:40 +0900

    wip

---
+++ test/bundle.js
@@ -0,0 +1,18 @@
+const test = require('ava')
+const path = require('path')
+const bundle = require('../lib/bundle')
+const tempfile = require('tempfile')
+const fs = require('fs')
+
+const COVER = path.join(__dirname, 'fixtures/cover.jpg')
+const IMGS = [...Array(2).keys()].map(n => path.join(__dirname, `fixtures/${n}.jpg`))
+const DEST = tempfile()
+
+test.after.always(() => fs.unlink(DEST, () => {}))
+
+test('bundle', async t => {
+    t.false(fs.existsSync(DEST))
+    await bundle({ pages: IMGS, cover: COVER }, DEST)
+    t.true(fs.existsSync(DEST))
+    // fs.copyFileSync(DEST, './test.pdf')
+})
--- test/capturePage.js
+++ test/capturePage.js
@@ -4,18 +4,18 @@ const capturePage = require('../lib/capturePage')
 
 const imgs = []
 const m = async pathUrl => {
-    const img = await capturePage(pathUrl)
-    imgs.push(img)
-    return img
+    const imgPath = await capturePage(pathUrl)
+    imgs.push(imgPath)
+    return imgPath
 }
 
 const noop = () => {}
 test.after.always(() => imgs.forEach(img => fs.unlink(img.path, noop)))
 
 test('capturePage', async t => {
-    const img = await m('/006962/ch01/01/')
+    const imgPath = await m('/006962/ch01/01/')
     // t.log(img)
-    t.true(fs.existsSync(img.path))
+    t.true(fs.existsSync(imgPath))
     // fs.copyFileSync(img.path, './test.jpg')
     t.pass()
 })
