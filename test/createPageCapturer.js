8de01cee15bbe0b73c56b86cf574586a319e9594
skt-t1-byungi <tiniwz@gmail.com>
2019-06-04 00:40:43 +0900

    wip

--- test/createPageCapturer.js
+++ test/createPageCapturer.js
@@ -6,7 +6,7 @@ const findChrome = require('chrome-finder')
 const createPageCapturer = require('../lib/createPageCapturer')
 const path = require('path')
 
-const getBrowser = createBrowserGetter(puppeteer, { executablePath: findChrome() })
+const getBrowser = createBrowserGetter(puppeteer, { executablePath: findChrome(), headless: true, slowMo: 0 })
 
 let browser
 let capturePage
@@ -26,12 +26,15 @@ test.after.always(() => {
 })
 
 test('capturePage', async t => {
-    const res1 = await m('/006993/part02/ch11/01-02/')
+    const res1 = await m('/006958/part01/ch02/01-02/')
     t.truthy(res1.nextUrl)
     t.true(fs.existsSync(res1.imgPath))
-    fs.copyFileSync(res1.imgPath, path.join(__dirname, 'snapshot/capturePage.png'))
+    fs.copyFileSync(res1.imgPath, path.join(__dirname, 'snapshot/capture1.png'))
+
+    fs.copyFileSync((await m('/006884/ch01/03/03-01/')).imgPath, path.join(__dirname, 'snapshot/capture2.png'))
+    fs.copyFileSync((await m('/006884/ch08/01/')).imgPath, path.join(__dirname, 'snapshot/capture3.png'))
 
     // end page
-    const res2 = await m('006884/ch10/08/')
-    t.falsy(res2.nextUrl)
+    const res4 = await m('006884/ch10/08/')
+    t.falsy(res4.nextUrl)
 })
